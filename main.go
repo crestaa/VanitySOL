68b6f04d6506cf2c1b36b0fd477286d652092952
Gabriele Crestanello <58698974+crestaa@users.noreply.github.com>
2024-06-01 14:43:18 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
+	"github.com/gagliardetto/solana-go/rpc/ws"
+)
+
+func generateKeyPair() (solana.PublicKey, solana.PrivateKey) {
+	privKey, err := solana.NewRandomPrivateKey()
+	if err != nil {
+		fmt.Printf("Error generating key pair: %v\n", err)
+		os.Exit(1)
+	}
+	return privKey.PublicKey(), privKey
+}
+
+func isMatch(address, start, end string, caseSensitive bool) bool {
+	if !caseSensitive {
+		address = strings.ToLower(address)
+		start = strings.ToLower(start)
+		end = strings.ToLower(end)
+	}
+	return strings.HasPrefix(address, start) && strings.HasSuffix(address, end)
+}
+
+func worker(start, end string, caseSensitive bool, results chan<- [2]string, wg *sync.WaitGroup, attempts *uint64, done <-chan struct{}) {
+	defer wg.Done()
+	for {
+		select {
+		case <-done:
+			return
+		default:
+			pubKey, privKey := generateKeyPair()
+			address := pubKey.String()
+			atomic.AddUint64(attempts, 1)
+			if isMatch(address, start, end, caseSensitive) {
+				results <- [2]string{address, privKey.String()}
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	var start, end string
+	var caseSensitive bool
+
+	fmt.Print("Enter the start of the address: ")
+	fmt.Scanln(&start)
+	fmt.Print("Enter the end of the address: ")
+	fmt.Scanln(&end)
+	fmt.Print("Case sensitive? (true/false): ")
+	fmt.Scanln(&caseSensitive)
+
+	wsClient, err := ws.Connect(context.Background(), rpc.MainNetBeta_WS)
+	if err != nil {
+		fmt.Printf("Error connecting to websocket: %v\n", err)
+		os.Exit(1)
+	}
+	defer wsClient.Close()
+
+	numWorkers := int(float64(runtime.NumCPU()) * float64(0.5))
+	results := make(chan [2]string)
+	done := make(chan struct{})
+	var wg sync.WaitGroup
+	var attempts uint64
+
+	go func() {
+		ticker := time.NewTicker(5 * time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				fmt.Printf("Number of attempts: %d\n", atomic.LoadUint64(&attempts))
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	fmt.Printf("Starting with %d threads\n", numWorkers)
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go worker(start, end, caseSensitive, results, &wg, &attempts, done)
+	}
+
+	foundResult := <-results
+	close(done)
+	fmt.Printf("Found matching address: %s\n", foundResult[0])
+	fmt.Printf("Private key: %s\n", foundResult[1])
+
+	close(results)
+	wg.Wait()
+}
